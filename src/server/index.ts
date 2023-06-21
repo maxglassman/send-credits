3c87051baabfe165be619678a4eb6d5c2a59f02c
Max Glassman <maxglassman@Maxs-MacBook-Pro.local>
2023-06-20 20:36:16 -0700

    added folder structure for react-app

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,7 +2,7 @@ import express, { Application } from 'express';
 import routes from './routes';
 
 const app: Application = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // Middleware
 app.use(express.json());
