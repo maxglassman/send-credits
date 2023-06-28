e3013709458cf88ba4d504e2b2eb3cd457bde862
Max Glassman <maxglassman@Maxs-MacBook-Pro.local>
2023-06-27 22:14:34 -0700

    cleaned up legacy issues from incorrect build process effecting client.

--- client/src/components/ConnectWalletButton.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const ConnectWalletButton = ({ connectWallet, }) => {
-    return (0, jsx_runtime_1.jsx)("button", { onClick: connectWallet, children: "Connect Wallet" });
-};
-exports.default = ConnectWalletButton;
